8c9453cd4eb3772ea08983460fc9a62cfd7925de
Victor LaBrie <vplabrie@gmail.com>
2022-11-26 21:47:21 -0600

    Creates vocabs for each sentiment with word frequency greater than n.

--- naive-bayes-classifier.py
+++ naive-bayes-classifier.py
@@ -10,7 +10,7 @@ import sys
 import re
 
 #First, we need to take in command line arguments.
-n = sys.argv[1]
+n = int(sys.argv[1])
 trainingFileArg = sys.argv[2]
 testFileArg = sys.argv[3]
 
@@ -19,6 +19,78 @@ testFileArg = sys.argv[3]
 # learned. 
 trainingFile = open(trainingFileArg)
 
-#After this we start our main function
-def main(n):
-    return 0
+#After this we start our training function
+def training():
+    #Vocab
+    positiveDict = {}
+    negativeDict = {}
+    #Number of each doc type
+    positiveReviews = 0
+    negativeReviews = 0
+    #number of words of each topic
+    positiveWordCount = 0
+    negativeWordCount = 0
+    #For every review, get word frequencies, and if negative add to negative model, do the same for positive
+    for review in trainingFile.readlines():
+        reviewWords = words(review)
+        if review.split()[1]=='1':
+            for wordTuple in reviewWords:
+                if wordTuple[0] in positiveDict:
+                    positiveDict[wordTuple[0]] += wordTuple[1]
+                else:
+                    positiveDict[wordTuple[0]] = wordTuple[1]
+            positiveReviews += 1
+            positiveWordCount += wordTuple[1]
+        else:
+            for wordTuple in reviewWords:
+                if wordTuple[0] in negativeDict:
+                    negativeDict[wordTuple[0]] += wordTuple[1]
+                else:
+                    negativeDict[wordTuple[0]] = wordTuple[1]
+            negativeReviews += 1
+            negativeWordCount += wordTuple[1]
+    print(positiveWordCount, negativeWordCount)
+    #if occurences less than N, remove. do this for both positive and negative models
+    removeKeys = []
+    for word in positiveDict.keys():
+        if positiveDict[word]<n:
+            removeKeys.append(word)
+    for key in removeKeys:
+        positiveDict.pop(key)
+    removeKeys = []
+    for word in negativeDict.keys():
+        if negativeDict[word]<n:
+            removeKeys.append(word)
+    for key in removeKeys:
+        negativeDict.pop(key)
+    return positiveDict, negativeDict, positiveWordCount, negativeWordCount, positiveReviews, negativeReviews
+
+
+
+#Function for getting word frequency
+def words(text) -> list:
+    wordDict = {}
+    #Look at each word
+    splitText = text.split();
+    #create list of all words
+    for word in splitText:
+        if word not in wordDict:
+            wordDict[word] = 1
+        else:
+            wordDict[word] += 1
+
+            
+    #next, we sort the dictionary
+    sortedWordList = sorted(wordDict, key = wordDict.get, reverse=True)
+    sortedWordDict = {}
+    for y in sortedWordList:
+        sortedWordDict[y]=wordDict[y]
+    sortedItems = list(sortedWordDict.items())
+    
+    return sortedItems[::-1]
+
+#call training function
+posModel, negModel, posWordCount, negWordCount, posReviewCount, negReviewCount = training()
+#test file not opened until after training complete
+testFile = open(testFileArg)
+
